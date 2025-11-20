17d8780e8b21c4d25abe4059376b15389c84b1b7
“pfhenley86” <fordp86@gmail.com>
2025-11-20 14:01:54 -0500

    completed the guessing game

--- CsharpNumberGuesser/Program.cs
+++ CsharpNumberGuesser/Program.cs
@@ -1,3 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+bool isCorrectGuess = false;
+Random random = new Random();
+
+int randomNum = random.Next(0, 11);
+
+Console.WriteLine("Welcome to the number guessing game!");
+Console.WriteLine("A number between 1 and 10 will be generated");
+Console.WriteLine("If you guess the correct number, you win!");
+
+Console.WriteLine(randomNum);
+
+while (!isCorrectGuess)
+{
+    Console.WriteLine("Please enter your guess.");
+    int guess = Convert.ToInt32(Console.ReadLine());
+
+    if (guess > randomNum)
+    {
+        Console.WriteLine("Your guess is too high");
+    }
+    else if (guess < randomNum)
+    {
+        Console.WriteLine("Your guess is too low");
+    }
+    else
+    {
+        Console.WriteLine("Correct!");
+    }
+}
+
+Console.ReadKey();
